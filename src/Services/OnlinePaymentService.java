7bfd428b170c5ed2ec9510f28dae1dc19ed04c2d
Thiago Antonio <thiagoapsx@gmail.com>
2023-10-18 21:59:56 -0300

    deleted the public from the interface

--- src/Services/OnlinePaymentService.java
+++ src/Services/OnlinePaymentService.java
@@ -2,7 +2,7 @@ package Services;
 
 public interface OnlinePaymentService {
 
-    public Double paymentFee(Double amount);
-    public Double interest(Double amount, Integer months);
+    Double paymentFee(Double amount); // ‘interfaces’ não precisam declarar se que é publico, pois presume-se que o método é publico e abstrato.
+    Double interest(Double amount, Integer months);
 
 }
